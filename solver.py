ecf3174ce32859efe0fce465009b7c610388fb75
Suryaprakash Vengadesan <surya@88-66-5a-1c-a5-ce.dhcp.lbnl.us>
2023-07-27 14:27:35 -0700

    init mdp steps

---
+++ solver.py
@@ -0,0 +1,46 @@
+'''
+Create MDP that takes in input data from WRENCH data
+
+Input: Graph of tasks 
+'''
+
+def extract_resources():
+    """
+    Extracts all available resources
+    """
+
+def extract_tasks():
+    """
+    Extracts a list of all tasks 
+    """
+
+def extract_task_graph():
+    """
+    Extracts the task graph from list of tasks 
+    """
+
+def create_states():
+    """
+    Create states for MDP
+    """
+
+def create_actions():
+    """
+    Create actions for MDP
+    """
+
+def assign_reward_function():
+    """
+    Determine cost of each resource type 
+
+    e.g. reference NERSC cost table for resource per hour
+    """
+
+def solve_MDP():
+    """
+    Create and solve a MDP
+    """
+
+
+
+
